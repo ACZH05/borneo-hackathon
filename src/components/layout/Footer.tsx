1dbe29afc4e53411dad779a73fef84b8f79fd5ea
njwongzhe <ngjaawei@gmail.com>
2026-03-04 15:44:26 +0800

    Responsive Update

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <footer className="sticky top-0 z-50 flex flex-wrap gap-4 items-center justify-between bg-surface shadow-sm min-w-screen px-8 py-3">
+    <footer className="sticky top-0 flex flex-wrap gap-6 items-center justify-between bg-surface shadow-sm min-w-screen px-4 py-4">
         <div className="flex items-center gap-3">
             {/* --- Logo --- */}
             <div className="flex items-center justify-center rounded-full bg-secondary/20 h-10 w-10">
@@ -14,7 +14,7 @@ export default function Footer() {
         </div>
 
         {/* --- Footer Links --- */}
-        <div className="flex items-center text-xs text-textGrey gap-3">
+        <div className="flex items-center text-xs text-textGrey gap-3 ml-auto">
             <span>Privacy</span>
             <span>Terms</span>
             <span>Accessibility</span>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,23 +2,27 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation"; 
+import { JSX, useState } from "react";
+
+// --- Navigation Link List ---
+const navLinks = [
+  { name: "Home", href: "/" },
+  { name: "Alerts", href: "/alerts" },
+  { name: "Resources", href: "/resources" },
+  { name: "Supports", href: "/supports" },
+  { name: "Settings", href: "/settings" },
+];
 
 export default function Header() {
   const pathname = usePathname(); 
 
-  // --- Navigation Link List ---
-  const navLinks = [
-    { name: "Home", href: "/" },
-    { name: "Alerts", href: "/alerts" },
-    { name: "Resources", href: "/resources" },
-    { name: "Supports", href: "/supports" },
-    { name: "Settings", href: "/settings" },
-  ];
-
   return (
-    <header className="sticky top-0 z-50 flex flex-wrap gap-4 items-center justify-between bg-surface shadow-sm min-w-screen px-8 py-3">
+    <header className="sticky top-0 z-50 flex flex-wrap gap-4 items-center justify-between bg-surface shadow-sm min-w-screen px-4 py-4">
       {/* --- Left Side --- */}
-      <div className="flex items-center gap-3">
+      <div className="flex items-center justify-center gap-3">
+        {/* --- Drawer for Mobile --- */}
+        <Drawer />
+
         {/* --- Logo --- */}
         <div className="flex items-center justify-center rounded-full bg-secondary/20 h-10 w-10">
           <span className="material-symbols-outlined text-2xl text-primary">spa</span> {/* Logo Icon from Google Material Symbols */}
@@ -31,8 +35,8 @@ export default function Header() {
         </div>
       </div>
 
-      {/* --- Middle Navigation Links --- */}
-      <nav className="flex items-center gap-1 rounded-full bg-foreground/3 px-1 py-1">
+      {/* --- Middle Navigation Links --- */} {/* Show only on desktop. */}
+      <nav className="hidden xl:flex items-center gap-1 rounded-full bg-foreground/3 px-1 py-1">
         {navLinks.map((link) => (
           <Link
             href={link.href}
@@ -47,9 +51,9 @@ export default function Header() {
         ))}
       </nav>
 
-      <div className="flex gap-4">
-        {/* --- Search Bar --- */}
-        <div className="flex items-center rounded-full bg-foreground/8 border-none px-3 py-1">
+      <div className="flex gap-4 ml-auto">
+        {/* --- Search Bar --- */} {/* Show only on desktop. */}
+        <div className="hidden xl:flex items-center rounded-full bg-foreground/8 border-none px-3 py-1"> 
           <span className="material-symbols-outlined text-textGrey/60 text-xs">search</span> {/* Search Icon from Google Material Symbols */}
           <input placeholder="Find help..." className="rounded-full bg-transparent border-none outline-none placeholder:text-textGrey/60 placeholder:text-xs px-2"></input>
         </div>
@@ -58,5 +62,57 @@ export default function Header() {
         <button className="flex items-center justify-center rounded-full border-primary border-2 text-primary font-bold px-5 py-1 transition-all duration-300 hover:bg-primary hover:text-white">Log in</button>
       </div>
     </header>
+  );
+}
+
+function Drawer() : JSX.Element {
+  const [isOpen, setIsOpen] = useState(false); 
+  const pathname = usePathname(); 
+
+  return (
+    <>
+      {/* --- Drawer Button --- */} {/* Show only on mobile. */}
+      <button onClick={() => setIsOpen(true)} className="xl:hidden flex items-center justify-center"> 
+        <span className="material-symbols-outlined text-3xl">menu</span>
+      </button>
+
+      {/* --- Drawer Overlay --- */}
+      <div 
+        className={`fixed inset-0 z-40 bg-black/50 transition-opacity duration-300 ease-out ${
+          isOpen ? "opacity-100 visible" : "opacity-0 invisible"
+        }`}
+        onClick={() => setIsOpen(false)} // Close drawer when clicking on the overlay.
+      />
+
+      {/* --- Drawer Panel --- */}
+      <aside 
+        className={`fixed top-0 left-0 z-50 flex flex-col bg-surface shadow-2xl h-full w-54 transition-transform duration-500 ease-out ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        {/* --- Search Bar --- */}
+        <div className="flex items-center rounded-full bg-foreground/8 border-none px-3 py-1 mx-2 my-4">
+          <span className="material-symbols-outlined text-textGrey/60 text-xs">search</span> {/* Search Icon from Google Material Symbols */}
+          <input placeholder="Find help..." className="rounded-full bg-transparent border-none outline-none placeholder:text-textGrey/60 placeholder:text-xs px-2"></input>
+        </div>
+
+        {/* --- Menu Links --- */}
+        <nav className="flex flex-col">
+          {navLinks.map((link) => (
+            <Link
+              href={link.href}
+              className={`text-sm font-bold px-4 py-3 ${
+                pathname === link.href 
+                  ? "bg-primary text-surface" 
+                  : "text-textGrey hover:bg-secondary/20"
+              }`}
+              onClick={() => setIsOpen(false)}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+      </aside>
+    </>
   );
 }
