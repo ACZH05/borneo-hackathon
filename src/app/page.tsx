a3619dbad38b279193b8560bdd6e92d81549f271
ACZH05 <alfredchin2005@gmail.com>
2026-03-07 17:34:26 +0800

    Add Latest Alert component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import ButtonListComponent from "./components/home/ButtonListComponent";
 import EmergencyDetailsComponent from "./components/home/EmergencyDetailsComponent";
+import LatestAlert from "./components/home/LatestAlert";
 
 export default function HomePage() {
   return (
-    <div className="grid grid-cols-4 grid-rows-4 gap-4 p-8">
+    <div className="grid grid-cols-4 grid-rows-4 gap-x-4 gap-y-8 p-8">
       <div className="col-span-2 row-span-2">
         <div className="flex flex-col gap-8">
           <div className="text-6xl font-bold">
@@ -22,7 +23,9 @@ export default function HomePage() {
         </div>
       </div>
       <div className="col-span-2 row-span-2 col-start-3 bg-blue-50">2</div>
-      <div className="col-span-3 row-span-2 row-start-3 bg-green-50">3</div>
+      <div className="col-span-3 row-span-2 row-start-3">
+        <LatestAlert />
+      </div>
       <div className="col-start-4 row-start-3 bg-yellow-50">4</div>
       <div className="col-start-4 row-start-4 bg-purple-50">5</div>
     </div>
